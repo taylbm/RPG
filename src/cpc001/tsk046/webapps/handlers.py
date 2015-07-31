a0715c5cc83cdcebb23edd3a60a6092081e84579
Brandon M. Taylor <Brandon.M.Taylor@noaa.gov>
2015-07-31 16:11:12 -0500

    added switch statements to simplify controlling behavior of hci

--- src/cpc001/tsk046/webapps/handlers.py
+++ src/cpc001/tsk046/webapps/handlers.py
@@ -31,7 +31,7 @@ def RS():
 	for cls in class_list:
 		temp = dict((getattr(getattr(_rpg.rdastatus,cls),x),x) for x in class_dict[cls])
 		if cls == 'rdastatus':
-			temp.update({0:'N/A'})
+			temp.update({0:'UNKNOWN'})
 		temp.update({-9999:'-9999'})
 		RS_states.update({cls:temp})
 		
@@ -46,8 +46,8 @@ def RS():
 		aux_gen_list.append('true')
 	else:	
 		aux_gen_list.append('false')	
-	alarm_list = [RS_states['alarmsummary'][key].strip('AS_') for key in RS_states['alarmsummary'].keys() if (key & RS_dict['RS_RDA_ALARM_SUMMARY']) > 0 or key == RS_dict['RS_RDA_ALARM_SUMMARY']] 
-	RS_dict.update({'RDA_static':{'TPS_STATUS':RS_states['tps'][RS_dict['RS_TPS_STATUS']].strip('TP_'),'OPERABILITY_LIST':",".join(oper_list),'AUX_GEN_LIST':"<br>".join(aux_gen_list),'RS_RDA_ALARM_SUMMARY_LIST':",".join(alarm_list),'RDA_STATE':RS_states['rdastatus'][RS_dict['RS_RDA_STATUS']].replace('RS_',''),'WIDEBAND':RS_states['wideband'][_rpg.liborpg.orpgrda_get_wb_status(0)].replace('RS_','')}})
+	alarm_list = [RS_states['alarmsummary'][key].strip('AS_-9999') for key in RS_states['alarmsummary'].keys() if (key & RS_dict['RS_RDA_ALARM_SUMMARY']) > 0 or key == RS_dict['RS_RDA_ALARM_SUMMARY']] 
+	RS_dict.update({'RDA_static':{'TPS_STATUS':RS_states['tps'][RS_dict['RS_TPS_STATUS']].strip('TP_'),'OPERABILITY_LIST':",".join(oper_list),'AUX_GEN_LIST':"<br>".join(aux_gen_list),'RS_RDA_ALARM_SUMMARY_LIST':"".join(alarm_list),'RDA_STATE':RS_states['rdastatus'][RS_dict['RS_RDA_STATUS']].replace('RS_',''),'WIDEBAND':RS_states['wideband'][_rpg.liborpg.orpgrda_get_wb_status(0)].replace('RS_','')}})
 	RS_dict.update({'RDA_alarms_all':[x.replace('AS_','') for x in RS_states['alarmsummary'].values() if not x.strip('-').isdigit()]})
 	return RS_dict
 def RPG():
