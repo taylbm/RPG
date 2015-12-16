1385e784bdcbd04fdc188ab5cd51cc46c422b6c7
Brandon M. Taylor <Brandon.M.Taylor@noaa.gov>
2015-12-16 14:59:05 -0600

    support for toggling channels for redundant sites -bt

--- web/dqd.js
+++ web/dqd.js
@@ -7,6 +7,7 @@ var timeInterval = {
 			'1Month':month/1000,
 			'6Months':then/1000
 };
+
 function mean(arr)
 {
     var i,
@@ -17,6 +18,7 @@ function mean(arr)
     }
     return sum / len;
 }
+
 function median(arr) 
 {
 
@@ -29,6 +31,7 @@ function median(arr)
     else
         return (arr[half-1] + arr[half]) / 2.0;
 }
+
 function dBMean(arr) 
 {
     var linearPow = arr.map(function(x) Math.pow(10,(x/10)));
@@ -36,11 +39,11 @@ function dBMean(arr)
     var convertedBack = 10 * Math.log10(linearMean);
     return convertedBack;
 }
-function setSizes(event, ui)
+
+function setSizes(event, ui, plotAdd)
 {
     if (!SummaryData)
         return;
- 
     var whichPage = ui.toPage.get(0).id;
     var method = whichPage.replace('-page', '');
     var possibleChartName = method + '-container';
@@ -53,66 +56,134 @@ function setSizes(event, ui)
     chartContainer.height('400').width($('#' + pageMainName).width() * 0.8).css('margin', 'auto');
     
     var belowDataToPlot = [],
-        aboveDataToPlot = []
+        aboveDataToPlot = [],
+	dailyPoints	= []
     ;
-    
-    if (method == 'bragg')        
-        $.each(SummaryData, function (idx, obj) {
-            belowDataToPlot.push([obj.time * 1e3, obj.medianBragg < 0 ? obj.medianBragg : null]);
-            aboveDataToPlot.push([obj.time * 1e3, obj.medianBragg >= 0 ? obj.medianBragg : null]);
-        });
-    else if (method == 'rain')
-        $.each(SummaryData, function (idx, obj) {
-            belowDataToPlot.push([obj.time * 1e3, obj.medianRain < 0 ? obj.medianRain : null]);
-            aboveDataToPlot.push([obj.time * 1e3, obj.medianRain >= 0 ? obj.medianRain : null]);
-        });
-    else if (method == 'snow')
-        $.each(SummaryData, function (idx, obj) {
-            belowDataToPlot.push([obj.time * 1e3, obj.medianSnow < 0 ? obj.medianSnow : null]);
-            aboveDataToPlot.push([obj.time * 1e3, obj.medianSnow >= 0 ? obj.medianSnow : null]);
-        });
-    
-    $.plot(
-        chartContainer, 
-        [
+    if(redundant){
+	var chan1 = {'belowDataToPlot':[],'aboveDataToPlot':[]}	
+	    chan2 = {'belowDataToPlot':[],'aboveDataToPlot':[]}
+	; 
+        if (method == 'bragg'){
+            $.each(SummaryData, function (idx, obj) {
+		if (obj.modeRedundant > -99) {
+		    var channel = 'chan'+obj.modeRedundant
+                    eval(channel)['belowDataToPlot'].push([obj.time * 1e3, obj.medianBragg < 0 ? obj.medianBragg : null]);
+                    eval(channel)['aboveDataToPlot'].push([obj.time * 1e3, obj.medianBragg >= 0 ? obj.medianBragg : null]);
+		}
+            });
+	    $.each(DailyData, function(idx, obj) {
+	        dailyPoints.push([obj.time * 1e3, obj.medianBragg]);
+	    });
+        } 	
+	else if (method == 'rain'){
+	    $.each(SummaryData, function (idx, obj) {
+		if (obj.modeRedundant > -99) {
+                    var channel = 'chan'+obj.modeRedundant
+                    eval(channel)['belowDataToPlot'].push([obj.time * 1e3, obj.medianRain < 0 ? obj.medianRain : null]);
+                    eval(channel)['aboveDataToPlot'].push([obj.time * 1e3, obj.medianRain >= 0 ? obj.medianRain : null]);
+                }
+	    });
+	    $.each(DailyData, function(idx, obj) {
+		dailyPoints.push([obj.time * 1e3, obj.medianRain]);
+	    });
+	}
+	else if (method == 'snow'){
+	    $.each(SummaryData, function (idx, obj) {
+		if (obj.modeRedundant > -99) {
+                    var channel = 'chan'+obj.modeRedundant
+                    eval(channel)['belowDataToPlot'].push([obj.time * 1e3, obj.medianSnow < 0 ? obj.medianSnow : null]);
+                    eval(channel)['aboveDataToPlot'].push([obj.time * 1e3, obj.medianSnow >= 0 ? obj.medianSnow : null]);
+                }
+	    });
+	    $.each(DailyData, function(idx, obj) {
+	        dailyPoints.push([obj.time * 1e3, obj.medianBragg]);
+	    });
+	}
+    }
+    var plotOpts =         
+	[
             {
-                id: 'topTolerance', 
-                data: [[then, 0.2], [now, 0.2]], 
+                id: 'topTolerance',
+                data: [[then, 0.2], [now, 0.2]],
                 lines:  {show: true, lineWidth: 0, fill: false}
-            }, 
+            },
             {
-                data: [[then, -0.2], [now, -0.2]], 
+                data: [[then, -0.2], [now, -0.2]],
                 lines:  {
-                    show: true, 
-                    lineWidth: 0, 
+                    show: true,
+                    lineWidth: 0,
                     fill: 0.2
-                }, 
-                olor: 'rgb(128, 128, 255)', 
+                },
+                color: 'rgb(128, 128, 255)',
                 fillBetween: 'topTolerance'
-            }, 
-            {
-                data: belowDataToPlot, 
+            }
+        ]
+    if (redundant){
+	for (p in plotAdd){
+            plotOpts.push(
+		{
+		    data: eval(plotAdd[p])['belowDataToPlot'],
+		    lines:    {
+			show: true,
+			fill: true,
+			fillColor:'rgb(0,0,128)'
+		    }
+		}
+	    );
+            plotOpts.push(
+		{
+		    data: eval(plotAdd[p])['aboveDataToPlot'],
+		    lines:    {
+			show: true,	
+			fill: true,
+			fillColor:'rgb(128,0,0)'
+		    }
+		}
+	    );
+    	}
+    }
+    else{
+	plotOpts.push(            
+	    {
+                data: belowDataToPlot,
                 lines:   {
                     show: true,
                     fill:   true,
                     fillColor:  'rgb(0, 0, 128)'
-                }, 
-                points: {
-                    show: false
-                }
-            },
-            {
-                data: aboveDataToPlot, 
-                lines:   {
-                    show: true,
-                    fill:   true,
-                    fillColor:  'rgb(128, 0, 0)'
-                }, 
+                },
                 points: {
                     show: false
                 }
             }
-        ], 
+	);
+	plotOpts.push(
+            {
+                data: aboveDataToPlot,
+                lines:   {
+                    show: true,
+                    fill:   true,
+                    fillColor:  'rgb(128, 0, 0)'
+                },
+                points: {
+                    show: false
+                }
+            }
+	);
+    }
+
+    plotOpts.push(
+        {
+            data:dailyPoints,
+            color:'black',
+            points: {
+                show: true,
+                symbol: "cross",
+            }
+        }
+    );
+	
+    $.plot(
+        chartContainer,plotOpts, 
         {
             yaxis:  {
                 min:    -0.5,
@@ -128,6 +199,7 @@ function setSizes(event, ui)
             }
         }
     );
+    
 }
 
 function normalizeToGage(val)
@@ -135,7 +207,7 @@ function normalizeToGage(val)
     return Math.max(0, Math.min(100, Math.round(val / 1.0 * 50 + 50)));
 }
 
-function determineOverview(flag,timePeriod)
+function determineOverview(time,chan)
 {
     var tBragg = -99,
         tSnow = -99,
@@ -144,25 +216,56 @@ function determineOverview(flag,timePeriod)
 	mSnow = [],
 	mRain = []
     ;
-  
-    if(flag){
-        $.each(SummaryData, function(idx,obj){
-    	    if(timeInterval[timePeriod] <= obj.time){
-                mBragg.push(obj.medianBragg);
-		mSnow.push(obj.medianSnow);
-		mRain.push(obj.medianRain);
-	    }
-        });
-        tRain = median(mRain.filter(function(val) {return val !== null;}))
-	tSnow = median(mSnow.filter(function(val) {return val !== null;}))
-	tBragg = median(mBragg.filter(function(val) { return val !== null;}))
+    if(redundant){
+	rBragg = {'chan1':[],'chan2':[]};
+        rSnow = {'chan1':[],'chan2':[]};
+        rRain = {'chan1':[],'chan2':[]};
 	
+	if(time != 'MostRecent'){
+            $.each(SummaryData, function(idx,obj){
+                if(timeInterval[time] <= obj.time){
+		    var channel = 'chan'+obj.modeRedundant
+                    rBragg[channel].push(obj.medianBragg);
+                    rSnow[channel].push(obj.medianSnow);
+                    rRain[channel].push(obj.medianRain);
+                }
+            });
+            tRain = median(rRain[chan].filter(function(val) {return val !== null;}))
+            tSnow = median(rSnow[chan].filter(function(val) {return val !== null;}))
+            tBragg = median(rBragg[chan].filter(function(val) { return val !== null;}))  
+        }
+        else {
+	    var copyData = JSON.parse(JSON.stringify(SummaryData)).reverse()
+	    $.each(copyData,function(idx,obj) {
+		if (obj.modeRedundant == chan.replace('chan','')) {
+                    tRain = obj.medianRain;
+            	    tSnow = obj.medianSnow;
+            	    tBragg = obj.medianBragg;
+		    return false;
+		}
+	    });
+	}
+		    
     }
-    else
-        tRain = SummaryData[SummaryData.length - 1].medianRain,
-        tSnow = SummaryData[SummaryData.length - 1].medianSnow,
-        tBragg = SummaryData[SummaryData.length - 1].medianBragg
-    ;
+    else {
+        if(time != 'MostRecent'){
+            $.each(SummaryData, function(idx,obj){
+                if(timeInterval[time] <= obj.time){
+                    mBragg.push(obj.medianBragg);
+                    mSnow.push(obj.medianSnow);
+                    mRain.push(obj.medianRain);
+                }
+            });
+            tRain = median(mRain.filter(function(val) {return val !== null;}))
+            tSnow = median(mSnow.filter(function(val) {return val !== null;}))
+            tBragg = median(mBragg.filter(function(val) { return val !== null;}))
+        }
+        else
+            tRain = SummaryData[SummaryData.length - 1].medianRain,
+            tSnow = SummaryData[SummaryData.length - 1].medianSnow,
+            tBragg = SummaryData[SummaryData.length - 1].medianBragg
+        ;
+    }
 		    	
     var valsForOverview = [];
     
